f3975dd5f7b11b80ba48afccd2cfb561ae21b56c
Mike <mike.nichols97@gmail.com>
2019-05-08 18:59:11 -0400

    Dates are now in the correct format with the makeTestDate function implemented

--- site.js
+++ site.js
@@ -38,7 +38,51 @@ const vue_app = new Vue({
             github: "https://github.com/mnichols17/219-p3"
       },
       methods: {
-            /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
+            makeTextDate: function(dateArray) {
+                  let month = "";
+                  switch (dateArray[1]){
+                        case 1:
+                              month = "January";
+                              break;
+                        case 2:
+                              month = "February";
+                              break;
+                        case 3:
+                              month = "March";
+                              break;
+                        case 4:
+                              month = "April";
+                              break;
+                        case 5:
+                              month = "May";
+                              break;
+                        case 6:
+                              month = "June";
+                              break;
+                        case 7:
+                              month = "July";
+                              break;
+                        case 8:
+                              month = "August";
+                              break;
+                        case 9:
+                              month = "September";
+                              break;
+                        case 10:
+                              month = "October";
+                              break;
+                        case 11:
+                              month = "November";
+                              break;
+                        case 12:
+                              month = "December";
+                              break;
+                        default:
+                              month = "N/A";
+                              break;
+                  }
+                  return `${month} ${dateArray[2]}, ${dateArray[0]}`;
+            }
       }
 })
 	
